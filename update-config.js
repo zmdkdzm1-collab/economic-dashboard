a47fca7db85b18b5026582dd7f413f2bf8592373
zmdkdzm1-collab <zmdkdzm1@gmail.com>
2026-08-07 16:43:58 +0900

    chore: 업데이트 탭 인증 설정(암호화 토큰)

--- update-config.js
+++ update-config.js
@@ -1,2 +1,2 @@
 // 이 파일은 '업데이트 탭 → 최초 설정'이 자동 생성합니다. 비밀번호로 암호화된 GitHub 토큰이며 직접 편집하지 마세요.
-window.UPDATE_ENC = "";
+window.UPDATE_ENC = "600000.Ws5XQ3duFTHrjLmjMYoUuA==./rrQaKLc2v0kvww1.lFETOuTXVJ0rR/MRLGkL5OYKK7KTtnAQ06TerHv0Vde9/JYmceJ4t081/Bm5IOKBr+HipM8iuk63mDK1OFv+utBM9hHMlRlVh3n5n5O6U4sLg/RER7MFoXu+Na23g0lvlnrlom+i1AWPo6Pazw==";
